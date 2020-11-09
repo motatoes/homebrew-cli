af052dbec2d8a0473e2880bb93cd884e3c9f9d40
Igor Zalutski <izalutski@gmail.com>
2020-11-09 14:23:37 +0000

    update bin.install

--- dg-formula.rb
+++ dg-formula.rb
@@ -6,6 +6,6 @@ class DgFormula < Formula
     url "https://digger-releases.s3-eu-west-1.amazonaws.com/dg-darwin-0.1.0.zip", :using => :curl
   
     def install
-      bin.install "innovid/hello-world"
+      bin.install "dg/dg"
     end
   end
