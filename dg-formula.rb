4c02108c1f4af1ed3cb29926bcb6acc0ab22d20a
Mohamed <moe.habib9@gmail.com>
2020-11-11 12:07:29 +0000

    Update dg-formula.rb

--- dg-formula.rb
+++ dg-formula.rb
@@ -3,7 +3,7 @@ class DgFormula < Formula
     homepage "https://digger.dev"
   
   
-    url "https://digger-releases.s3-eu-west-1.amazonaws.com/dg-darwin-0.1.0.zip", :using => :curl
+    url "https://digger-releases.s3-eu-west-1.amazonaws.com/dg-darwin-0.1.1.zip", :using => :curl
     
     bottle :unneeded
     
